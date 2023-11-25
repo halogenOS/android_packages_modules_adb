a12f14f3ebda46b486a3f91e7508953fe026c11a
Beru Krugermeier <windowz414@gnuweeb.org>
2023-11-25 10:55:47 +0100

    Revert "Allow ADB root on eng and userdebug"
    This reverts commit ba1d2df1c43cca00abf1059b701ec87203b7dd09.

--- root/adbroot_service.cpp
+++ root/adbroot_service.cpp
@@ -40,7 +40,6 @@ namespace adbroot {
 using ::android::AutoMutex;
 using ::android::base::ReadFileToString;
 using ::android::base::SetProperty;
-using ::android::base::GetProperty;
 using ::android::base::Trim;
 using ::android::base::WriteStringToFile;
 
@@ -68,7 +67,7 @@ ndk::ScopedAStatus ADBRootService::isSupported(bool* _aidl_return) {
     }
 
     AutoMutex _l(lock_);
-    *_aidl_return = GetProperty("ro.build.type", "user") != "user";
+    *_aidl_return = __android_log_is_debuggable();
     return ndk::ScopedAStatus::ok();
 }
 
